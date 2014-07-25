286d182ed6c5d5e5c7c4122170db44621fae31ea
tenorok <mail@tenorok.ru>
2014-07-25 22:28:12 +0400

    Метод resolveAbsolutePath

--- modules/Cli.js
+++ modules/Cli.js
@@ -1,10 +1,27 @@
+const path = require('path');
+
 /**
  * Модуль для упрощения написания CLI.
  *
  * @constructor
  */
-function Cli(modules) {}
+function Cli() {}
 
 Cli.prototype = {};
+
+/**
+ * Получить абсолютный путь из относительного
+ * или массив абсолютных путей из массива относительных.
+ *
+ * @param {string|string[]} relativePath Относительный путь или массив путей
+ * @returns {string|string[]}
+ */
+Cli.resolveAbsolutePath = function(relativePath) {
+    return Array.isArray(relativePath)
+        ? relativePath.map(function(relativePath) {
+            return path.join(process.cwd(), relativePath);
+        })
+        : path.join(process.cwd(), relativePath);
+};
 
 module.exports = Cli;
--- test/testCli.js
+++ test/testCli.js
@@ -1,4 +1,18 @@
-const assert = require('chai').assert,
+const path = require('path'),
+    assert = require('chai').assert,
     Cli = require('../modules/Cli');
 
-describe('Модуль Cli.', function() {});
+describe('Модуль Cli.', function() {
+
+    it('Метод resolveAbsolutePath: один путь', function() {
+        assert.equal(Cli.resolveAbsolutePath('/test/fixtures/'), path.join(__dirname, '../test/fixtures/'));
+    });
+
+    it('Метод resolveAbsolutePath: несколько путей', function() {
+        assert.deepEqual(
+            Cli.resolveAbsolutePath(['/test/1/', '/test/2/']),
+            [path.join(__dirname, '../test/1/'), path.join(__dirname, '../test/2/')]
+        );
+    });
+
+});
