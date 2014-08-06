9f16106eb93eddd1621dc455e6d171f39d08fdda
tenorok <mail@tenorok.ru>
2014-08-06 22:11:08 +0400

    index.js экспортирует все модули

---
+++ index.js
@@ -0,0 +1,15 @@
+/*!
+ * node-bemaker
+ * @copyright Artem Kurbatov, tenorok.ru
+ * @license MIT license
+ */
+module.exports = {
+    Cli: require('./modules/Cli'),
+    Depend: require('./modules/Depend'),
+    fs: require('./modules/fs'),
+    Join: require('./modules/Join'),
+    Log: require('./modules/Log'),
+    Make: require('./modules/Make'),
+    Pool: require('./modules/Pool'),
+    Walk: require('./modules/Walk')
+};
